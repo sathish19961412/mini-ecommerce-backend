13c1f8e811a33cef528a205687f1c77dc0fb6d48
sathish19961412 <sathishfreelanc5.com>
2024-07-11 11:13:27 -0400

    create database connection in mongodb

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express=require('express');
 const app=express();
 const path=require('path');
+const connectDatabase=require('./config/connectDatabase');
 const dotenv=require('dotenv');
 dotenv.config({path: path.join(__dirname,'config','config.env')})
 
 const products=require('./routes/products');
 const orders=require('./routes/order')
+
+connectDatabase();
 
 app.use('/api/v1/',products);
 app.use('/api/v1/',orders);
